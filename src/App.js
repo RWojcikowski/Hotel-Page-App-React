3ec0b30434c4cc50d1d22260a259d2694ef76024
Radosław Wójcikowski <radoslaw.chicqen@gmail.com>
2022-05-17 22:50:24 +0200

    application state implementation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
+import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Menu from './components/Menu/Menu';
 import Hotels from './components/Hotels/Hotels';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Menu />
-      <Hotels />
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    hotels: [
+      {
+        id: 1,
+        name: 'Pod Gruszą ',
+        city: 'Warszawa ',
+        rating: ' 8.3 ',
+        discription: ' Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle',
+        image: ''
+      },
+      {
+        id: 2,
+        name: 'Debowy Sad ',
+        city: 'Wrocław ',
+        rating: ' 8.8 ',
+        discription: ' Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle',
+        image: ''
+      }
+
+    ],
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Menu />
+        <Hotels hotels={this.state.hotels} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Hotels/Hotel/Hotel.js
+++ src/components/Hotels/Hotel/Hotel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Hotel.module.css';
 import hotelImg from '../../../assets/images/hotel.jpg';
 
-function Hotel() {
+function Hotel(props) {
   return (
     <div className={`card ${styles.hotel}`}>
       <div className="card-body">
@@ -17,11 +17,11 @@ function Hotel() {
           <div className="col-8">
             <div className="row">
               <div className="col">
-                <p className={styles.title}>Pensjonat</p>
-                <span className="badge badge-light">Warszawa</span>
+                <p className={styles.title}>{props.name}</p>
+                <span className="badge badge-light">{props.city}</span>
               </div>
               <div className="col text-right">
-                <h5>Ocena: 8.3</h5>
+                <h5>Ocena:{props.rating}</h5>
                 <a href="#" className="btn btn-primary mt-2 px-4">Pokaż</a>
               </div>
             </div>
@@ -29,7 +29,7 @@ function Hotel() {
 
           <div className="col-12">
             <p className={styles.description}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque consequat id lorem vitae accumsan.
+              {props.discription}
             </p>
           </div>
         </div>
