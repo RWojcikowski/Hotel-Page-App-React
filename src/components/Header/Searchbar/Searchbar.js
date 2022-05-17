1920a46c4eeb940d96e89d0e50162ec5bf80adb6
Radosław Wójcikowski <radoslaw.chicqen@gmail.com>
2022-05-17 19:49:04 +0200

    init logic searchBar

--- src/components/Header/Searchbar/Searchbar.js
+++ src/components/Header/Searchbar/Searchbar.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 function Searchbar() {
+    const [term, setTerm] = useState()
+
+    const search = () => {
+        console.log('szukaj')
+    }
+    const updateTerm = (e) => {
+
+    }
     return (
         <div className="d-flex">
             <input
-
+                value={term}
+                onChange={updateTerm}
                 className=""
                 type="text"
                 placeholder="Szukaj..." />
 
             <div className="col-12">
                 <button
+                    onClilck={search}
                     className="ml-2 btn btn-info">Szukaj</button>
 
             </div>
