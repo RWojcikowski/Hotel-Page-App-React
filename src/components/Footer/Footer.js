e7612124eb898e627062963891c7d737f53f5128
Radosław Wójcikowski <radoslaw.chicqen@gmail.com>
2022-05-20 18:57:05 +0200

    Create Layout for page and remove duplicate Classname

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,6 @@
+const Footer = () => (
+    <div className="text-center m-3">Hotel-page-app 2022</div>
+
+)
+
+export default Footer;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+function Layout(props) {
+    return (
+        <div>
+            <div>{props.header}</div>
+            <div className=" container">
+                {props.menu}
+            </div>
+            <div className=" container">
+                {props.content}
+            </div>
+            <div>{props.footer}</div>
+        </div>
+    )
+};
+
+
+export default Layout;
