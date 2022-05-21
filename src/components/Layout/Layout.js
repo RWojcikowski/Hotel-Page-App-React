21cf1e221c018cd75e94929e19b491f5f95750c3
Radosław Wójcikowski <radoslaw.chicqen@gmail.com>
2022-05-21 14:14:51 +0200

    add hoc to layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,18 @@
+import withClass from "../hoc/withClass";
+
 function Layout(props) {
   return (
-    <div>
+    <>
       <div>{props.header}</div>
       <div className="container">
-          {props.menu}  
+        {props.menu}
       </div>
       <div className="container">
         {props.content}
       </div>
       <div>{props.footer}</div>
-    </div>
+    </>
   );
 }
 
-export default Layout;
+export default withClass(Layout, 'layout');
