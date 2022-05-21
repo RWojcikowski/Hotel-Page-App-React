e2f8fb579a240d5a60d17737945ad4fa4817d28d
Radosław Wójcikowski <radoslaw.chicqen@gmail.com>
2022-05-21 14:44:42 +0200

    Mouse tracking  logic

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './Header.module.css';
+import withMousePosition from '../hoc/withMousePosition';
 
 function Header(props) {
   return (
     <header className={`${styles.header}`}>
+      <div className='text-info'>
+        {props.mouseX} <br></br>
+        {props.mouseY}
+      </div>
       {props.children}
     </header>
   );
 }
 
-export default Header;
+export default withMousePosition(Header);
