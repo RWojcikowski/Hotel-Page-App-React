cf1de204698421885b4483b5318b8c2142ac1c0f
Radosław Wójcikowski <radoslaw.chicqen@gmail.com>
2022-05-25 13:26:49 +0200

    add timer for  Best offer

--- src/components/Hotels/Hotel/BestHotel/BestHotel.js
+++ src/components/Hotels/Hotel/BestHotel/BestHotel.js
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import moment from "moment";
+
 const BestHotel = (props) => {
+    const endTime = moment().add(23, 'minutes').add(35, 'seconds')
+    const [time, setTime] = useState('')
     const hotel = props.getHotel({ minHotels: 1 });
+
+    setInterval(() => {
+        const leftTime = -moment().diff(endTime) / 1000;
+        const minutes = Math.floor(leftTime / 60);
+        const seconds = Math.floor(leftTime % 60);
+        setTime(`minut; ${minutes}, sekund: ${seconds}`);
+        console.log(leftTime);
+    }, 1000)
 
     if (!hotel) return null;
 
@@ -11,6 +24,7 @@ const BestHotel = (props) => {
                         <h6 className="card-title">{hotel.name}</h6>
                         <p> Ocena : {hotel.rating}</p>
                     </div>
+                    <p> do konca oferty zostało : {time}</p>
                     <a href="#" className="btn-sm btn-light">
                         Pokaz
                     </a>
